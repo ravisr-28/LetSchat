d866c7099665b626712e2c0383b8535c209c4083
ravisr-28 <ravimacos35490@gamil.com>
2026-01-23 21:33:10 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRouter from "./routes/auth_routes.js";
+import path from "path";
 
 dotenv.config()
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth",authRouter)
+app.use("/api/auth",authRouter);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+}
 app.listen(PORT,()=>{
     console.log("App is listening on: " +PORT)
 })
