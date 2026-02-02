b5bec01d830a06d687c75ec352bb08341d41bb40
ravisr-28 <ravimacos35490@gamil.com>
2026-02-02 19:36:08 +0530

    welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const {MONGO_URI}   = process.env;
+        const {MONGO_URI} = ENV;
         if(!MONGO_URI){
             throw new Error("MONGO_URI is not defined in environment variables");
         }
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(ENV.MONGO_URI);
         console.log("MongoDB connected successfully");  
     } catch (error) {
         console.error("MongoDB connection failed:", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,17 @@
 import express from "express"
-import dotenv from "dotenv"
 import authRouter from "./routes/auth_routes.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
-
-dotenv.config()
+import { ENV } from "./lib/env.js";
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 app.use(express.json());
 app.use("/api/auth",authRouter);
 
-if(process.env.NODE_ENV === "production"){
+if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 }
 app.listen(PORT,()=>{
