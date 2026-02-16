c5756b924d22b0feb9c38a7a646fcbef7153bf05
ravisr-28 <ravimacos35490@gamil.com>
2026-02-16 19:24:46 +0530

    login&logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
+  const { logout } = useAuthStore();
   return (
-    <div>
-      
+    <div className="z-10">
+      <h1>Chat Page</h1>
+      <button onClick={logout}>Logout</button>
     </div>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+import AnimatedBorderContainer from "../components/AnimatedBorderContainer";
+import {
+  MessageCircleIcon,
+  LockIcon,
+  MailIcon,
+  LoaderIcon,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, isLoggingIn } = useAuthStore();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(formData);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-3xl md:h-[600px] h-[550px]">
+        <AnimatedBorderContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            {/* Form Section Left side*/}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                {/* Heading Text */}
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Welcome Back
+                  </h2>
+                  <p>Login to access to your account</p>
+                </div>
+                {/* Form */}
+                <form onSubmit={handleSubmit} className='space-y-6'>
+                  {/* Email Input */}
+                  <div>
+                    <label className="auth-input-label">Email</label>
+                    <div className="relative">
+                      <MailIcon className="auth-input-icon" />
+
+                      <input
+                        type="email"
+                        value={formData.email}
+                        onChange={(e) =>
+                          setFormData({ ...formData, email: e.target.value })
+                        }
+                        className="input"
+                        placeholder="ravi@gmail.com"
+                      />
+                    </div>
+                  </div>
+                  {/* Password Input */}
+                  <div>
+                    <label className="auth-input-label">Password</label>
+                    <div className="relative">
+                      <LockIcon className="auth-input-icon" />
+
+                      <input
+                        type="password"
+                        value={formData.password}
+                        onChange={(e) =>
+                          setFormData({ ...formData, password: e.target.value })
+                        }
+                        className="input"
+                        placeholder="Enter your password"
+                      />
+                    </div>
+                  </div>
+                  {/* Submit Button */}
+                  <button
+                    className="auth-btn"
+                    type="submit"
+                    disabled={isLoggingIn}
+                  >
+                    {isLoggingIn ? (
+                      <LoaderIcon className="w-full h-5 animate-spin text-center" />
+                    ) : (
+                      "Login"
+                    )}
+                  </button>
+                </form>
+                <div className="mt-6 text-center">
+                  <Link to="/signup" className="auth-link">
+                    Don't have an account? Sign Up
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* Image Section Right side*/}
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent">
+              <div>
+                <img
+                  src="/login.png"
+                  alt="People using LetSchat"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="mt-6 text-center">
+                  <h3 className="text-xl font-medium text-purple-400">
+                    Connect anytime, anywhere with LetSchat
+                  </h3>
+                  <div className="mt-4 flex justify-center gap-4">
+                    <span className="auth-badge">Free</span>
+                    <span className="auth-badge">Easy Setup</span>
+                    <span className="auth-badge">Private</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </AnimatedBorderContainer>
+      </div>
     </div>
-  )
+  );
 }
 
 export default LoginPage
