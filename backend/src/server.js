a6df129f549e0ba4d8f1c312d152fd07777e299b
ravisr-28 <ravimacos35490@gamil.com>
2026-02-27 21:07:04 +0530

    production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,13 +13,16 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieparser());
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 server.listen(PORT, () => {
   console.log("App is listening on: " + PORT);
