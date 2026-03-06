d949b0bbee453e3c446b0af47dfe1cd0bab6f42f
ravisr-28 <ravimacos35490@gamil.com>
2026-03-06 16:17:08 +0530

    fix express wildcard route

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,9 +18,10 @@ app.use(cookieparser());
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
