183eb8053cd72b6caabb8c8423dff144de6e7e3a
ravisr-28 <ravimacos35490@gamil.com>
2026-03-15 12:35:32 +0530

    fixed error of login failed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://192.168.1.55:3000/api"
+      ? `http://${window.location.hostname}:3000/api`
       : "/api",
   withCredentials: true,
 });
