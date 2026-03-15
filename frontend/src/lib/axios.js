4679cf77e197663abd69978d9995f67b8e0cf16f
ravisr-28 <ravishankarx213@gmail.com>
2026-03-15 13:02:41 +0530

    fixed error facing during login

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? `http://${window.location.hostname}:3000/api`
-      : "/api",
+      : `${import.meta.env.VITE_API_URL || ""}/api`,
   withCredentials: true,
 });
